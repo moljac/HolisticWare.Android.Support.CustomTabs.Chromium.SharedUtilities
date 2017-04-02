433dca91b2e964208dfd6480a80e10d7ca11f053
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-04-02 23:22:03 +0200

    AppCompat package CustomTabActivityHelper refactoring and API improvements

--- source/Android.Support.CustomTabs.Chromium.SharedUtilities/CustomTabActivityHelper.cs
+++ source/Android.Support.CustomTabs.Chromium.SharedUtilities/CustomTabActivityHelper.cs
@@ -25,157 +25,196 @@ namespace Android.Support.CustomTabs.Chromium.SharedUtilities
 	/// This is a helper class to manage the connection from Activity to the CustomTabs 
     /// Service.
 	/// </summary>
-	public class CustomTabActivityHelper : Java.Lang.Object, IServiceConnectionCallback
+	public partial class CustomTabActivityHelper : Java.Lang.Object, IServiceConnectionCallback
 	{
-		private CustomTabsSession mCustomTabsSession;
-		private CustomTabsClient mClient;
-		private CustomTabsServiceConnection mConnection;
-		private IConnectionCallback mConnectionCallback;
+        private CustomTabsSession custom_tabs_session;
+        private CustomTabsClient custom_tabs_client;
+        private CustomTabsServiceConnection custom_tabs_service_connection;
+        private IConnectionCallback connection_callback;
 
-		/// <summary>
-		/// Opens the URL on a Custom Tab if possible. Otherwise fallsback to opening it on a WebView.
-		/// </summary>
-		/// <param name="activity"> The host activity. </param>
-		/// <param name="customTabsIntent"> a CustomTabsIntent to be used if Custom Tabs is available. </param>
-		/// <param name="uri"> the Uri to be opened. </param>
-		/// <param name="fallback"> a CustomTabFallback to be used if Custom Tabs is not available. </param>
-        public static void OpenCustomTab(Activity activity, CustomTabsIntent customTabsIntent, Uri uri, ICustomTabFallback fallback)
-		{
-			string packageName = PackageManagerHelper.GetPackageNameToUse(activity, uri.Path);
+        /// <summary>
+        /// Opens the URL on a Custom Tab if possible. Otherwise fallsback to opening it on a WebView.
+        /// </summary>
+        /// <param name="activity"> The host activity. </param>
+        /// <param name="custom_tabs_intent"> a CustomTabsIntent to be used if Custom Tabs is available. </param>
+        /// <param name="uri"> the Uri to be opened. </param>
+        /// <param name="fallback"> a CustomTabFallback to be used if Custom Tabs is not available. </param>
+        public /*static*/ void LaunchUrlWithCustomTabsOrFallback
+                                (
+                                    Activity activity,
+                                    CustomTabsIntent custom_tabs_intent,
+                                    Uri uri,
+                                    ICustomTabFallback fallback
+                                )
+        {
+            string packageName = PackageManagerHelper.GetPackageNameToUse(activity, uri.Path);
 
-			//If we cant find a package name, it means theres no browser that supports
-			//Chrome Custom Tabs installed. So, we fallback to the webview
-			if (packageName == null)
-			{
-				if (fallback != null)
-				{
-					fallback.OpenUri(activity, uri);
-				}
-			}
-			else
-			{
-				customTabsIntent.Intent.SetPackage(packageName);
-				customTabsIntent.LaunchUrl(activity, uri);
-			}
+            //If we cant find a package name, it means theres no browser that supports
+            //Chrome Custom Tabs installed. So, we fallback to the webview
+            if (packageName == null)
+            {
+                if (fallback != null)
+                {
+                    fallback.OpenUri(activity, uri);
+                }
+            }
+            else
+            {
+                custom_tabs_intent.Intent.SetPackage(packageName);
+                custom_tabs_intent.LaunchUrl(activity, uri);
+            }
 
             return;
-		}
+        }
 
-		/// <summary>
-		/// Unbinds the Activity from the Custom Tabs Service. </summary>
-		/// <param name="activity"> the activity that is connected to the service. </param>
-        public virtual void UnbindCustomTabsService(Activity activity)
-		{
-			if (mConnection == null)
-			{
-				return;
-			}
-			activity.UnbindService(mConnection);
-			mClient = null;
-			mCustomTabsSession = null;
-			mConnection = null;
-		}
 
-		/// <summary>
-		/// Creates or retrieves an exiting CustomTabsSession.
-		/// </summary>
-		/// <returns> a CustomTabsSession. </returns>
-		public virtual CustomTabsSession Session
-		{
-			get
-			{
-				if (mClient == null)
-				{
-					mCustomTabsSession = null;
-				}
-				else if (mCustomTabsSession == null)
-				{
+        /// <summary>
+        /// Creates or retrieves an exiting CustomTabsSession.
+        /// </summary>
+        /// <returns> a CustomTabsSession. </returns>
+        public virtual CustomTabsSession Session
+        {
+            get
+            {
+                if (custom_tabs_client == null)
+                {
+                    custom_tabs_session = null;
+                }
+                else if (custom_tabs_session == null)
+                {
                     /*
                     public CustomTabsSession NewSession(OnNavigationEventDelegate onNavigationEventHandler);
                     public virtual CustomTabsSession NewSession(CustomTabsCallback callback);
                     public CustomTabsSession NewSession
                                                 (
-                                                    OnNavigationEventDelegate onNavigationEventHandler, 
-                                                    ExtraCallbackDelegate extraCallbackHandler
+                                                    OnNavigationEventDelegate onNavigationEventHandler,
+                                                    CustomTabsClient.ExtraCallbackDelegate extraCallbackHandler
                                                 );
                     */
-                    mCustomTabsSession = mClient.NewSession    
-                                                    (   
-                                                        //null                                                        
-                                                        null,
-                                                        null
-                                                    );
-				}
-				return mCustomTabsSession;
-			}
-		}
+                    custom_tabs_session = custom_tabs_client.NewSession
+                                                (
+                                                    // OnNavigationEventDelegate onNavigationEventHandler,
+                                                    null,
+                                                    // CustomTabsClient.ExtraCallbackDelegate extraCallbackHandler
+                                                    null
+                                                );
+                }
+                return custom_tabs_session;
+            }
+        }
 
-		/// <summary>
-		/// Register a Callback to be called when connected or disconnected from the Custom Tabs Service. </summary>
-		/// <param name="connectionCallback"> </param>
-		public virtual void setConnectionCallback(IConnectionCallback connectionCallback)
-		{
-			this.mConnectionCallback = connectionCallback;
-		}
+        public CustomTabsClient.OnNavigationEventDelegate NavigationEventHandler
+        {
+            get;
+            set;
+        } = null;
 
-		/// <summary>
-		/// Binds the Activity to the Custom Tabs Service. </summary>
-		/// <param name="activity"> the activity to be binded to the service. </param>
+        public CustomTabsClient.ExtraCallbackDelegate ExtraCallback
+        {
+            get;
+            set;
+        } = null;
+
+        /// <summary>
+        /// Register a Callback to be called when connected or disconnected from the Custom Tabs Service. 
+        /// </summary>
+        public virtual IConnectionCallback ConnectionCallback
+        {
+            get;
+            set;
+        }
+
+        public string UriTest
+        {
+            get;
+            set;
+        } = "http://xamarin.com";
+
+        /// <summary>
+        /// Binds the Activity to the Custom Tabs Service. </summary>
+        /// <param name="activity"> the activity to be binded to the service. </param>
         public virtual void BindCustomTabsService(Activity activity)
-		{
-			if (mClient != null)
-			{
-				return;
-			}
+        {
+            if (custom_tabs_client != null)
+            {
+                return;
+            }
 
-			string packageName = PackageManagerHelper.GetPackageNameToUse(activity, "http://xamarin.com");
-			if (packageName == null)
-			{
-				return;
-			}
+            string packageName = PackageManagerHelper.GetPackageNameToUse(activity, this.UriTest);
 
-			mConnection = new ServiceConnection(this);
-			CustomTabsClient.BindCustomTabsService(activity, packageName, mConnection);
-		}
+            if (packageName == null)
+            {
+                return;
+            }
 
-		/// <seealso cref= <seealso cref="CustomTabsSession#mayLaunchUrl(Uri, Bundle, List)"/>. </seealso>
-		/// <returns> true if call to mayLaunchUrl was accepted. </returns>
-        public virtual bool MayLaunchUrl(Uri uri, Bundle extras, IList<Bundle> otherLikelyBundles)
-		{
-			if (mClient == null)
-			{
-				return false;
-			}
+            custom_tabs_service_connection = new ServiceConnection(this);
+            CustomTabsClient.BindCustomTabsService(activity, packageName, custom_tabs_service_connection);
 
-			CustomTabsSession session = Session;
-			if (session == null)
-			{
-				return false;
-			}
+            return;
+        }
 
-			return session.MayLaunchUrl(uri, extras, otherLikelyBundles);
-		}
+        /// <summary>
+        /// Unbinds the Activity from the Custom Tabs Service. </summary>
+        /// <param name="activity"> the activity that is connected to the service. </param>
+        public virtual void UnbindCustomTabsService(Activity activity)
+        {
+            if (custom_tabs_service_connection == null)
+            {
+                return;
+            }
+            activity.UnbindService(custom_tabs_service_connection);
+            custom_tabs_client = null;
+            custom_tabs_session = null;
+            custom_tabs_service_connection = null;
 
-		public virtual void OnServiceConnected(CustomTabsClient client)
-		{
-			mClient = client;
-			mClient.Warmup(0L);
-			if (mConnectionCallback != null)
-			{
-				mConnectionCallback.OnCustomTabsConnected();
-			}
-		}
+            return;
+        }
 
-		public virtual void OnServiceDisconnected()
-		{
-			mClient = null;
-			mCustomTabsSession = null;
-			if (mConnectionCallback != null)
-			{
-				mConnectionCallback.OnCustomTabsDisconnected();
-			}
-		}
+        /// <summary>
+        /// Warmup 
+        /// Tells the browser of a likely future navigation to a URL.
+        /// </summary>
+        /// <seealso cref= <seealso cref="CustomTabsSession#mayLaunchUrl(Uri, Bundle, List)"/>. </seealso>
+        /// <returns> true if call to mayLaunchUrl was accepted. </returns>
+        public virtual bool MayLaunchUrl(Uri uri, Bundle extras, IList<Bundle> other_likely_bundles)
+        {
+            if (custom_tabs_client == null)
+            {
+                return false;
+            }
 
+            CustomTabsSession cts = this.Session;
+
+            if (cts == null)
+            {
+                return false;
+            }
+
+            return cts.MayLaunchUrl(uri, extras, other_likely_bundles);
+        }
+
+        public virtual void OnServiceConnected(CustomTabsClient client)
+        {
+            custom_tabs_client = client;
+            custom_tabs_client.Warmup(0L);
+            if (connection_callback != null)
+            {
+                connection_callback.OnCustomTabsConnected();
+            }
+
+            return;
+        }
+
+        public virtual void OnServiceDisconnected()
+        {
+            custom_tabs_client = null;
+            custom_tabs_session = null;
+            if (connection_callback != null)
+            {
+                connection_callback.OnCustomTabsDisconnected();
+            }
+
+            return;
+        }
 	}
-
 }
